4bc1bdbf56985163967898418c2fc18c202ea353
devDzign <chabour.mourad87@gmail.com>
2020-10-12 15:19:57 +0200

    feature(sign in):
    - add Badge github actions

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Customer;
+use App\Entity\Producer;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        $producer = new Producer();
+
+        $producer
+            ->setPassword($this->userPasswordEncoder->encodePassword($producer, 'password'))
+            ->setFirstname('Jane')
+            ->setLastname('Doe')
+            ->setDisplayName('producer')
+            ->setEmail('producer@mail.com');
+
+        $manager->persist($producer);
+
+        $customer = new Customer();
+
+        $customer
+            ->setPassword($this->userPasswordEncoder->encodePassword($producer, 'password'))
+            ->setFirstname('Jane')
+            ->setLastname('Doe')
+            ->setDisplayName('customer')
+            ->setEmail('customer@mail.com');
+
+        $manager->persist($producer);
+
+        $manager->flush();
+    }
+}
