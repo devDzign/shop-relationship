b46c8748764873de61d27270eaec0efa540b891c
devDzign <chabour.mourad87@gmail.com>
2020-10-13 23:15:52 +0200

    fix(add rabbitmq):
    - add docker

--- src/Controller/WelcomeDevController.php
+++ src/Controller/WelcomeDevController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Message\Query\GetTotalUsersCount;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeDevController extends AbstractController
@@ -10,8 +14,18 @@ class WelcomeDevController extends AbstractController
     /**
      * @Route("/welcome", name="welcome_dev")
      */
-    public function welcome()
+    public function welcome(MessageBusInterface $queryBus)
     {
-        return $this->render('welcome_dev/index.html.twig');
+
+        $envelope = $queryBus->dispatch(new GetTotalUsersCount());
+        /** @var HandledStamp $handled */
+        $handled = $envelope->last(HandledStamp::class);
+
+        return $this->render('welcome_dev/index.html.twig', [
+            "var_react" => [
+                     "message" => "super valeur recuperer de php",
+                     "nbUsers" => $handled->getResult()
+                ]
+        ]);
     }
 }
---
+++ src/MessageHandler/Query/GetTotalUsersCountHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\MessageHandler\Query;
+
+
+use App\Message\Query\GetTotalUsersCount;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class GetTotalUsersCountHandler implements MessageHandlerInterface
+{
+
+
+    public function __invoke(GetTotalUsersCount $getTotalUsersCount)
+    {
+        return 50;
+    }
+}
