64aa45ac53c6f0a9f7a4e6d77a41b1fc5c54f7a3
devDzign <chabour.mourad87@gmail.com>
2020-10-23 23:53:02 +0200

    Feature(farm extra):
     -  add map.
     -  form farm

--- src/Entity/Producer.php
+++ src/Entity/Producer.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity()
@@ -18,6 +19,8 @@ class Producer extends User
      * @ORM\OneToOne(targetEntity=Farm::class, inversedBy="producer", cascade={"persist", "remove"})
      */
     private $farm;
+
+
 
 
     public function getRoles(): array
